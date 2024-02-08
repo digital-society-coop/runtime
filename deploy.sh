db6daa4b079a89be17f8329eb36d7499e70083c3
Chris Connelly <chris@connec.co.uk>
2024-02-08 12:25:06 +0000

    refactor: simplify `terraform-env.sh`
    This is now only responsible for setting up backend configuration for
    `init`. Deployment-specific flags are now set by `deploy.sh`. This makes
    it easier to use locally.

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ echo >&2
 eval "$(./terraform-env.sh "$service" "$environment")"
 
 echo -n "- Initialising terraform... " >&2
-if ! result="$(terraform init)"; then
+if ! result="$(terraform init -input=false -lockfile=readonly)"; then
 	echo 'failed' >&2
 	echo >&2
 	echo "$result" >&2
@@ -28,7 +28,7 @@ fi
 echo 'done' >&2
 
 echo -n "- Running terraform apply... " >&2
-if ! result="$(terraform apply)"; then
+if ! result="$(terraform apply -auto-approve -input=false)"; then
 	echo 'failed' >&2
 	echo >&2
 	echo "$result" >&2
