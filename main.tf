ae44a9fc9b38937618e9b94654168452b56e0931
Chris Connelly <chris@connec.co.uk>
2024-02-08 17:14:31 +0000

    feat: add `runtime` module with ingress controller
    This uses the fully open `ingress-nginx` controller.

--- main.tf
+++ main.tf
@@ -12,10 +12,42 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
   }
 }
 
 provider "digitalocean" {}
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.this.endpoint
+    token = digitalocean_kubernetes_cluster.this.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+    )
+  }
+
+  experiments {
+    manifest = true
+  }
+}
+
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.this.endpoint
+  token = digitalocean_kubernetes_cluster.this.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  )
+}
 
 variable "environment" {
   type = string
@@ -67,4 +99,12 @@ resource "digitalocean_kubernetes_cluster" "this" {
     size       = var.kubernetes_default_node_pool_size
     node_count = var.kubernetes_default_node_pool_node_count
   }
+}
+
+module "runtime" {
+  source = "./runtime"
+
+  environment  = var.environment
+  service      = var.service
+  cluster_name = digitalocean_kubernetes_cluster.this.name
 }
---
+++ runtime/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+  }
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "service" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+data "digitalocean_kubernetes_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+resource "kubernetes_namespace" "this" {
+  metadata {
+    name = var.service
+  }
+}
