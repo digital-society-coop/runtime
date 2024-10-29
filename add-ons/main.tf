0eedbc1f0effa618ecd01b393cfca7209a1dadf1
Endre Kadas <ekadas@gmail.com>
2024-10-29 23:43:37 +0000

    fix: Configure S3 backend for add-ons

--- add-ons/main.tf
+++ add-ons/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+
   required_providers {
     helm = {
       source  = "hashicorp/helm"
