465aa25b48b63051de4928798360c858bb6b77d3
Endre Kadas <ekadas@gmail.com>
2024-12-17 17:58:17 +0000

    feat: Add opentelemetry operator

--- add-ons/services/deploy-observe.sh
+++ add-ons/services/deploy-observe.sh
@@ -15,13 +15,18 @@ BASE64_ROOT_OBSERVE_PASSWORD=$(echo -n "$OBSERVE_ROOT_USER_PASSWORD" | openssl b
 export BASE64_ROOT_OBSERVE_PASSWORD
 
 # shellcheck disable=SC2016
-variables='$SERVICE
+opentelemetry_operator_variables='$NAMESPACE'
+
+envsubst "$opentelemetry_operator_variables" <opentelemetry-operator.yaml | kubectl apply -f -
+
+# shellcheck disable=SC2016
+openobserve_variables='$SERVICE
   $NAMESPACE
   $MANAGED_DOMAIN
   $OBSERVE_ROOT_USER_EMAIL
   $BASE64_ROOT_OBSERVE_PASSWORD'
 
-envsubst "$variables" <observe.yaml | kubectl apply -f -
+envsubst "$openobserve_variables" <observe.yaml | kubectl apply -f -
 
 echo >&2
 echo 'Deployment complete' >&2
