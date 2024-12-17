dbf706a21b81fd41552cc8d2c9bd943d0f7174e7
Endre Kadas <ekadas@gmail.com>
2024-12-17 19:36:32 +0000

    feat: Add openobserve collector

--- add-ons/services/deploy-observe.sh
+++ add-ons/services/deploy-observe.sh
@@ -9,22 +9,23 @@ echo >&2
 
 kubectl apply -f kube-state-metrics.yaml
 
-export MANAGED_DOMAIN="$SERVICE.$RUNTIME_DOMAIN"
-BASE64_ROOT_OBSERVE_PASSWORD=$(echo -n "$OBSERVE_ROOT_USER_PASSWORD" | openssl base64 -A)
-
-export BASE64_ROOT_OBSERVE_PASSWORD
-
 # shellcheck disable=SC2016
 opentelemetry_operator_variables='$NAMESPACE'
 
 envsubst "$opentelemetry_operator_variables" <opentelemetry-operator.yaml | kubectl apply -f -
+
+export MANAGED_DOMAIN="$SERVICE.$RUNTIME_DOMAIN"
+BASE64_ROOT_OBSERVE_PASSWORD=$(echo -n "$OBSERVE_ROOT_USER_PASSWORD" | openssl base64 -A)
+BASE64_OBSERVE_TOKEN=$(echo -n "$OBSERVE_ROOT_USER_EMAIL:$OBSERVE_ROOT_USER_PASSWORD" | openssl base64 -A)
+export BASE64_ROOT_OBSERVE_PASSWORD BASE64_OBSERVE_TOKEN
 
 # shellcheck disable=SC2016
 openobserve_variables='$SERVICE
   $NAMESPACE
   $MANAGED_DOMAIN
   $OBSERVE_ROOT_USER_EMAIL
-  $BASE64_ROOT_OBSERVE_PASSWORD'
+  $BASE64_ROOT_OBSERVE_PASSWORD
+  $BASE64_OBSERVE_TOKEN'
 
 envsubst "$openobserve_variables" <observe.yaml | kubectl apply -f -
 
